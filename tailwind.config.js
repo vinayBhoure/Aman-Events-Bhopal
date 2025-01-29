fb7a0beac742c83609fad2c45b2a4042d41319a0
unknown <bhoure21@gmail.com>
2025-01-30 00:11:11 +0530

    changing css and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-collage': "url('/src/assets/Hero.png')",
+      }
+    },
   },
   plugins: [],
 }
